d43359af7dc0e653bcde55f7ca496cd3b21b04ea
alfi9087 <95138007+alfi9087@users.noreply.github.com>
2024-09-28 20:26:04 +0700

    Update main.blade.php

--- resources/views/kasir/layout/main.blade.php
+++ resources/views/kasir/layout/main.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Corona Admin</title>
+  <title>Laundry Admin</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
